1525bbf1d1060631fd7c7fecfbf77dbe21be5d5c
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2022-11-29 22:06:09 -0300

    update

--- etl_api/controller/controller.go
+++ etl_api/controller/controller.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"runtime"
 )
 
 // Answer ficará responsável por responder a requisição
@@ -16,12 +17,47 @@ func Answer(w http.ResponseWriter, r *http.Request) {
 
 // process escopo global para o processamento
 func process() {
+	runtime.GOMAXPROCS(8)
+
 	numbers := getNumbers()
-	splitList(numbers.Number)
+
+	cut := len(numbers) / 10
+	numbers, s1 := divideList(numbers, cut)
+
+	cut = len(numbers) / 8
+	numbers, s2 := divideList(numbers, cut)
+
+	cut = len(numbers) / 6
+	numbers, s3 := divideList(numbers, cut)
+
+	cut = len(numbers) / 4
+	numbers, s4 := divideList(numbers, cut)
+
+	c := make(chan []int)
+	go sortNUmbersLowerToBigger(s1, c)
+	go sortNUmbersLowerToBigger(s2, c)
+	go sortNUmbersLowerToBigger(s3, c)
+	go sortNUmbersLowerToBigger(s4, c)
+
+	sorted1 := <-c
+	sorted2 := <-c
+	sorted3 := <-c
+	sorted4 := <-c
+
+	var finalList []int
+	finalList = append(finalList, sorted1...)
+	finalList = append(finalList, sorted2...)
+	finalList = append(finalList, sorted3...)
+	finalList = append(finalList, sorted4...)
+	finalList = append(finalList, numbers...)
+
+	finalList = sortNumbers(finalList)
+	fmt.Println(finalList)
+
 }
 
 // getNumbers faz a requisição para a outra API local que fornece os dados
-func getNumbers() models.NumbersList {
+func getNumbers() []int {
 	response, err := http.Get("http://127.0.0.1:5000/numbers")
 	if err != nil {
 		log.Fatal(err)
@@ -38,10 +74,62 @@ func getNumbers() models.NumbersList {
 		log.Fatal(err)
 	}
 
-	return numberList
+	return numberList.Number
 }
 
-// splitList reparte a lista obtida
-func splitList(numbers []int) {
-	fmt.Println(numbers[0:3])
+// divideList reparte a lista obtida
+func divideList(list []int, cut int) (tmp, slice []int) {
+	tmp = list[cut:]
+	slice = list[:cut]
+	return tmp, slice
+}
+
+// sortNUmbersBiggerToLower ordena os numeros do maior para o menor
+func sortNUmbersLowerToBigger(numbersList []int, c chan []int) {
+	for j := 0; j < len(numbersList); j++ {
+		for i := 0; i < len(numbersList); i++ {
+
+			var temp int
+			currentPosition := i
+			nextPostion := i + 1
+
+			if nextPostion == len(numbersList) {
+				break
+			}
+
+			if numbersList[currentPosition] < numbersList[nextPostion] {
+				temp = numbersList[currentPosition]
+				numbersList[currentPosition] = numbersList[nextPostion]
+				numbersList[nextPostion] = temp
+
+			}
+		}
+
+	}
+	c <- numbersList
+}
+
+// sortNumbers ordena os numeros do maior para o menor
+func sortNumbers(numbersList []int) []int {
+	for j := 0; j < len(numbersList); j++ {
+		for i := 0; i < len(numbersList); i++ {
+
+			var temp int
+			currentPosition := i
+			nextPostion := i + 1
+
+			if nextPostion == len(numbersList) {
+				break
+			}
+
+			if numbersList[currentPosition] < numbersList[nextPostion] {
+				temp = numbersList[currentPosition]
+				numbersList[currentPosition] = numbersList[nextPostion]
+				numbersList[nextPostion] = temp
+
+			}
+		}
+
+	}
+	return numbersList
 }
